386ee38863b67a5447a8e08f2c2c3c977f5200a2
JorgeDPSilva <63512109+JorgeDPSilva@users.noreply.github.com>
2020-04-11 18:51:03 +0100

    Update User.java
    Tudo comentadinho

--- src/User.java
+++ src/User.java
@@ -1,17 +1,31 @@
 public class User {
 
-    private String nome;
-    private String email;
+    // Variáveis de instância
+    private String nome;       
+    private String email;     
     private String password;
     private Coordenadas posicao;
 
+    /**
+     * Construtor parametrizado da classe user.
+     * @param Nome
+     * @param Email
+     * @param password
+     * @param posicao
+     * @return
+     */
     public User(String n, String mail, String pass, Coordenadas x){
-        setEmail(mail);
         setNome(n);
+        setEmail(mail);
         setPassword(pass);
         setPosicao(x);
     }
 
+     /**
+     * Construtor de cópia da classe User.
+     * @param User a
+     * @return
+     */
     public User(User a){
         this.nome = a.getNome();
         this.email = a.getEmail();
@@ -19,43 +33,94 @@ public class User {
         this.posicao = a.getPosicao();
     }
 
+    /**
+     * Devolve o nome do user.
+     * @param
+     * @return nome
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Atualiza o nome do user.
+     * @param Nome
+     * @return
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Devolve o email do user.
+     * @param
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Atualiza o mail do user.
+     * @param Email
+     * @return
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Devolve a password do user.
+     * @param
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Atualiza a password do user.
+     * @param Password
+     * @return
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public Coordenadas getPosicao() {
+     /**
+     * Devolve as coordenados do user.
+     * @param
+     * @return posicao
+     */
+     public Coordenadas getPosicao() {
         return posicao;
     }
 
+     /**
+     * Atualiza a Posicao do user.
+     * @param Posicao
+     * @return
+     */
     public void setPosicao(Coordenadas posicao) {
         this.posicao = posicao.clone();
     }
 
+     /**
+     * Método que faz uma cópia da classe User.
+     * Para tal invoca o construtor de cópia.
+     * @param
+     * @return User clone da classe User que recebe a mensagem.
+     */
     public User clone(){
         return new User(this);
     }
 
-    public boolean equals(Object o){
+    /**
+     * Método que verifica se a classe User é igual à classe User que recebe a mensagem.
+     * @param Object
+     * @return boolean
+     */
+      public boolean equals(Object o){
         if (o==this)return true;
         if ((o==null) || o.getClass()!=this.getClass())return false;
 
