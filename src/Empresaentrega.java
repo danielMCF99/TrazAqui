8a4a143fb7b523213e68883f4b00a47867024dcf
JorgeDPSilva <63512109+JorgeDPSilva@users.noreply.github.com>
2020-04-12 20:12:59 +0100

    update incompleto

--- src/Empresaentrega.java
+++ src/Empresaentrega.java
@@ -3,7 +3,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Empresaentrega extends User {
-
+     
+    // variáveis de instância
     private boolean prontaReceber;
     private double taxa;
     private int capacidade;
@@ -13,18 +14,21 @@ public class Empresaentrega extends User {
     private boolean vistoMedico;
     private List<Encomenda> encomendas;
 
-    public Empresaentrega(){
-        super();
-        this.prontaReceber = false;
-        this.taxa = 0;
-        this.capacidade = 0;
-        this.velocidade = 0;
-        this.classificacao = 0;
-        this.raio = 0;
-        this.vistoMedico = false;
-        this.encomendas = new ArrayList<Encomenda>();
-    }
-
+     /**
+     * Construtor parametrizado da classe Voluntario.
+     * @param Nome
+     * @param email
+     * @param password
+     * @param posicao
+     * @param classificao
+     * @param disponivel
+     * @param range
+     * @param velocidade
+     * @param verificado
+     * @param entregas_feitas
+     * @param encomendas
+     * @return
+     */
     public Empresaentrega(String nome,String email,String password,Coordenadas posicao,boolean pR,double taxa,int capacidade, double vel,double classi, double raio,boolean vM, List<Encomenda> enc){
         super(nome,email,password,posicao);
         this.prontaReceber = pR;
@@ -39,17 +43,17 @@ public class Empresaentrega extends User {
 
     public Empresaentrega(Empresaentrega emp){
         super(emp);
-        this.prontaReceber = emp.isProntaReceber();
+        this.prontaReceber = emp.getProntaReceber();
         this.taxa = emp.getTaxa();
         this.capacidade = emp.getCapacidade();
         this.velocidade = emp.getVelocidade();
         this.classificacao = emp.getClassificacao();
         this.raio = emp.getRaio();
-        this.vistoMedico = emp.isVistoMedico();
+        this.vistoMedico = emp.getVistoMedico();
         setEncomendas(emp.getEncomendas());
     }
 
-    public boolean isProntaReceber() {
+    public boolean getProntaReceber() {
         return prontaReceber;
     }
 
@@ -97,7 +101,7 @@ public class Empresaentrega extends User {
         this.raio = raio;
     }
 
-    public boolean isVistoMedico() {
+    public boolean getVistoMedico() {
         return vistoMedico;
     }
 
@@ -116,5 +120,58 @@ public class Empresaentrega extends User {
         }
         this.encomendas = aux;
     }
+
+     /**
+     * Método que faz uma cópia da classe Empresaentrega.
+     * Para tal invoca o construtor de cópia.
+     * @param
+     * @return User clone da classe Empresaentrega
+     */
+    public Empresaentrega clone(){
+        return new Empresaentrega(this);
+    }
+
+   /**
+     * Método que devolve a representação em String da classe Empresaentrega.
+     * @param
+     * @return String
+     */
+    
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empresa de entrega:\n");
+        sb.append("Nome: ").append(super.getNome()).append("\n");
+        sb.append("Email: ").append(super.getEmail()).append("\n");
+        sb.append("Password: ").append(super.getPassword()).append("\n");
+        sb.append("Posicao: ").append(super.getPosicao()).append("\n");
+        sb.append("\tPronta a receber: ").append(this.getProntaReceber()).append("\n");
+        sb.append("\tTaxa: ").append(this.getCoeficiente_Fiscal()).append("\n");
+        sb.append("\tCapacidade: ").append(this.getCapacidade()).append("\n");
+        sb.append("\tVelocidade: ").append(this.getVelocidade()).append("\n");
+        sb.append("\tClassificacao: ").append(this.getClassificacao()).append("\n");
+        sb.append("\tRaio: ").append(this.getRaio()).append("\n");
+        sb.append("\tVisto Medico: ").append(this.getVistoMedico()).append("\n");
+        sb.append("\tLista de encomendas: ").append(this.encomendas.toString()).append("\n");
+        return sb.toString();
+    }
+
+     /**
+     * Método que verifica se um Object é igual à classe Empresaentrega atual.
+     * @param Object
+     * @return boolean
+     */
+      public boolean equals(Object o){
+        if (o==this)return true;
+        if ((o==null) || o.getClass()!=this.getClass())return false;
+
+        Empresaentrega emp = (Empresaentrega) o;
+
+       return (super(emp).equals(super(this));
+    }
+
+
+
+
 
 }
