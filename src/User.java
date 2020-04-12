dc7c09126b6f524835eaf7a833164c8c9dba874e
JorgeDPSilva <63512109+JorgeDPSilva@users.noreply.github.com>
2020-04-12 20:40:16 +0100

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,3 +1,11 @@
+package Projeto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
 public class User {
 
     // Variáveis de instância
